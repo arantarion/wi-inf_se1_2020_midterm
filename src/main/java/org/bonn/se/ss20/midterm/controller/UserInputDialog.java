fec4dbb6571c1db8df856e75471dba8c4607c4c3
Henry Weckermann <henry.weckermann@smail.inf.h-brs.de>
2020-06-09 15:11:49 +0200

    reworked for command-pattern

---
+++ src/main/java/org/bonn/se/ss20/midterm/controller/UserInputDialog.java
@@ -0,0 +1,66 @@
+package org.bonn.se.ss20.midterm.controller;
+
+import org.bonn.se.ss20.midterm.command.*;
+import org.bonn.se.ss20.midterm.model.Container;
+import org.bonn.se.ss20.midterm.view.Console;
+
+import java.util.Arrays;
+
+/**
+ * @author Henry Weckermann, Anton Drees
+ */
+
+public class UserInputDialog {
+
+    private static final double VERSION = 1.2;
+
+    public UserInputDialog() {
+        setUpCommands();
+    }
+
+    private void setUpCommands() {
+        Container.getInstance().addCommand("enter", new EnterCommand());
+        Container.getInstance().addCommand("dump", new DumpCommand());
+        Container.getInstance().addCommand("dumpUndone", new DumpUndoneCommand());
+        Container.getInstance().addCommand("load", new LoadCommand());
+        Container.getInstance().addCommand("store", new StoreCommand());
+        Container.getInstance().addCommand("analyze", new AnalyzeCommand());
+        Container.getInstance().addCommand("help", new HelpCommand());
+        Container.getInstance().addCommand("undo", new UndoCommand());
+        Container.getInstance().addCommand("exit", new ExitCommand());
+        Container.getInstance().addCommand("addElement", new AddElementCommand());
+        Container.getInstance().addCommand("actors", new ActorCommand());
+        Container.getInstance().addCommand("status", new StatusCommand());
+        Container.getInstance().addCommand("clear", new ClearConsoleCommand());
+    }
+
+    public void start() {
+        System.out.println("User Story creation and management tool - Version " + VERSION);
+        System.out.println("Copyright (C) Henry Weckermann & Anton Drees");
+        System.out.println("License: Open Source");
+        System.out.println("Current user: " + System.getProperty("user.name"));
+        System.out.println("\nTo display the help text enter \"help\" below\n" +
+                "or for a specific command \"help <command>\"");
+
+        String userInput = null;
+
+        while (true) {
+            try {
+                userInput = Console.readConsole();
+            } catch (Exception ex) {
+                System.out.println("Something has gone horribly wrong");
+                ex.printStackTrace();
+            }
+
+            String[] commands;
+            if (userInput != null) {
+                commands = userInput.split(" ");
+
+                if (commands.length > 0) {
+                    Container.getInstance().getCommand(commands[0]).execute(Arrays.copyOfRange(commands, 1, commands.length));
+                }
+
+            }
+        }
+    }
+}
